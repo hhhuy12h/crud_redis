b9d8ee4a3ea51a65dbbc05054f4039731d22b9a1
hhhuy12h <Sothunhat28@gmail.com>
2021-11-11 02:18:34 -0800

    Huy

---
+++ src/main/java/com/example/crud_redis/CrudRedisApplication.java
@@ -0,0 +1,30 @@
+package com.example.crud_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+@Configuration
+@SpringBootApplication
+public class CrudRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRedisApplication.class, args);
+    }
+    @Bean
+    LettuceConnectionFactory jedisConnectionFactory(){
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    RedisTemplate redisTemplate(){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+
+}
